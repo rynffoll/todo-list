981ce6ba0ba7222b1ca6ee102fdbcb78360bc1f5
Ruslan Kamashev <rynffoll@gmail.com>
2017-05-13 22:25:10 +0400

    Small refactoring

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -9,23 +9,42 @@ import {Link} from "react-router-dom";
 export default class HomePage extends Component {
 
   calculateProgress = (category, tasks) => {
-    // todo: move filtering by category to wrapper? or something else?
     const filteredTasks = tasks.filter(x => x.category === category);
     return (filteredTasks.filter(x => x.done).length / filteredTasks.length) * 100;
   };
 
-  search = () => this.props.history.push({
-    pathname: this.props.location.pathname,
-    search: queryString.stringify({
-      done: this.searchCheckbox.checked,
-      query: this.searchQuery.value
-    })
-  });
+  searchByStatus = () => {
+    const {history, location} = this.props;
+    const search = queryString.parse(location.search);
+    history.push({
+      pathname: location.pathname,
+      search: queryString.stringify({
+        ...search,
+        done: this.searchCheckbox.checked
+      })
+    });
+  };
 
-  clearSearch = () => this.props.history.push({
-    pathname: this.props.location.pathname,
-    search: ""
-  });
+  searchByQuery = () => {
+    const {history, location} = this.props;
+    const search = queryString.parse(location.search);
+    history.push({
+      pathname: location.pathname,
+      search: queryString.stringify({
+        ...search,
+        query: this.searchQuery.value
+      })
+    });
+  };
+
+  clearSearch = () => {
+    this.props.history.push({
+      pathname: this.props.location.pathname,
+      search: ""
+    });
+    this.searchQuery.value = "";
+    this.searchCheckbox.checked = false;
+  };
 
   addCategory = () => {
     this.props.categoryActions.add(this.addCategoryInput.value);
@@ -37,20 +56,20 @@ export default class HomePage extends Component {
     this.addTaskInput.value = "";
   };
 
-  // todo: fixme
   filter = (searchParams) => (item) => {
-    if (searchParams.done === undefined
-      && searchParams.query === undefined) {
-      return true; // ignore filter
-    } else {
-      console.log(
-        "item",
-        item, item.done === (searchParams.done === "true"),
-        item.title.includes(searchParams.query, "i")
-      );
-      return item.done === (searchParams.done === "true")
-        && item.title.includes(searchParams.query);
+    let result = true;
+
+    if (searchParams.done !== undefined) {
+      const done = (searchParams.done === 'true');
+      result = result && item.done === done;
     }
+
+    if (searchParams.query !== undefined) {
+      const regExp = new RegExp(searchParams.query, "i");
+      result = result && regExp.test(item.title);
+    }
+
+    return result;
   };
 
   render() {
@@ -79,7 +98,7 @@ export default class HomePage extends Component {
                   <InputGroup.Addon>
                     <Checkbox inline
                               inputRef={ref => this.searchCheckbox = ref}
-                              onChange={this.search}
+                              onChange={this.searchByStatus}
                     >
                       Show done
                     </Checkbox>
@@ -87,7 +106,7 @@ export default class HomePage extends Component {
                   <FormControl type="text"
                                placeholder="Search task.."
                                inputRef={ref => this.searchQuery = ref}
-                               onChange={this.search}
+                               onChange={this.searchByQuery}
                   />
                   <InputGroup.Addon>
                     <Glyphicon glyph="remove" onClick={this.clearSearch}/>
